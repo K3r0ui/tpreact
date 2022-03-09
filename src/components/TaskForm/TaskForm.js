aaa79b56c8ecb36b6c145b06cef552e6fdbd2f49
Amine Karoui <aminkarouidev@gmail.com>
2022-03-09 16:45:40 +0100

    TP3 Fini

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,26 +1,39 @@
-import './TaskForm.css'
-  export function TaskForm(props){
-    const addTask = "Add a task";
-    const steps =[ " Enter a title" , " click on the button" ]
-    //props.say()
-    return(     
+import "./TaskForm.css";
+import { useState } from "react";
+export function TaskForm(props) {
+  const addTask = "Add a task";
+  const steps = [" Enter a title", " click on the button"];
+  const [title, setTitle] = useState("");
+  console.log("title", title);
+  function handleAddTask() {
+    props.addTask(title);
+  }
+  //props.say()
+  return (
     <>
-    <ul>
-    {steps.map(function(x,index) {
-      return ( <li key={index}>{x}</li>)
-    })}
-  
-        {steps.map(step =>  (
-      ( <li style={{color : "blue"}}>{step}</li>)
+      <ul>
+        {steps.map(function (x, index) {
+          return <li key={index}>{x}</li>;
+        })}
+
+        {steps.map((step) => (
+          <li style={{ color: "blue" }}>{step}</li>
         ))}
-
-    </ul>
-    <form action="" className="form">
-    <input type="text" name="task" id=""/>
-    <button type="submit">{addTask}</button>
-</form> 
-<h1> AMINOS</h1>
-</>
-
-)
+      </ul>
+      <form action="" className="form">
+        <input
+          type="text"
+          name="title"
+          id=""
+          value={title}
+          onChange={(x) => setTitle(x.target.value)}
+        />
+        <button type="button" onClick={handleAddTask}>
+          {addTask}
+        </button>
+        <h1>{title}</h1>
+      </form>
+      <h1> AMINOS</h1>
+    </>
+  );
 }
--- src/pages/TaskPage.js
+++ src/pages/TaskPage.js
@@ -1,45 +1,62 @@
-import './TaskPage.css'
+import "./TaskPage.css";
 //import {Task} from '../components/Task/Task';
-import {TaskForm} from '../components/TaskForm/TaskForm';
-import TaskList from '../components/TaskList/TaskList';
-export default function TaskPage(){
-  const tasks= [
-    {
-      id:1,
-      title:"learn HTML",
-      duration:60,
-      startDate:"2022-12-12",
-    },
-      {
-      id:2,
-      title:"learn CSS",
-      duration:60,
-      startDate:"2012-12-12",
+import { TaskForm } from "../components/TaskForm/TaskForm";
+import TaskList from "../components/TaskList/TaskList";
+import { useState } from "react";
 
+export default function TaskPage() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "learn HTML",
+      duration: 60,
+      type: "IT",
+      startDate: "2022-12-12",
     },
     {
-      id:3,
-      title:"learn REACT",
-      duration:10,
-      startDate:"2032-12-12"
+      id: 2,
+      title: "learn CSS",
+      duration: 60,
+      type: "IT",
+      startDate: "2012-12-12",
+    },
+    {
+      id: 3,
+      title: "learn REACT",
+      duration: 10,
+      type: "IT",
+      startDate: "2032-12-12",
+    },
+  ]);
 
-    }
-  ] 
   const loading = false;
-  function sayHello(){
-    alert("sayHelo")
+  function sayHello() {
+    alert("sayHelo");
   }
+  function addTask(title) {
+    setTasks([...tasks, { id: tasks.length + 1, title: title }]);
+    //setTaks([...tasks,{id:tasks.length}])
+  }
+  function toogleVisibility() {
+    setIsVisible(!isVisible);
+  }
+  console.log(isVisible);
   return (
-
     <div className="App">
-      
-{loading ? ( <div> loading... </div> ) : (
-  <>
-<TaskForm say={sayHello}/>
-<TaskList  myTasks={tasks}/>
-</>
-)}
+      <button onClick={toogleVisibility}>Toogle</button>
+      {loading ? (
+        <div> loading... </div>
+      ) : (
+        isVisible && (
+          <>
+            <TaskForm addTask={addTask} />
+            {/* <TaskForm say={sayHello} /> */}
+            <TaskList myTasks={tasks} />
+          </>
+        )
+      )}
     </div>
   );
-
-} 
+}
